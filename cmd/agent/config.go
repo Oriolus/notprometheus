6723d8175672b86416228731320ff94dacdac96d
Ivan Sinitsin <isinitsin@ozon.ru>
2023-07-15 22:45:42 +0400

    [iter5] test error fix

--- cmd/agent/config.go
+++ cmd/agent/config.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -23,6 +25,11 @@ const (
 	baseDefault              = ""
 	reportIntervalSecDefault = 10
 	pollIntervalSecDefault   = 2
+
+	addressEnvParamName        = "SERVER_ADDRESS"
+	baseEnvParamName           = "BASE_URL"
+	pollIntervalEnvParamName   = "POLL_INTERVAL"
+	reportIntervalEnvParamName = "REPORT_INTERVAL"
 )
 
 var defaultConfig = &config{
@@ -50,5 +57,38 @@ func parseFlags() (*config, error) {
 	cfg.reportInterval = time.Duration(reportIntervalSec) * time.Second
 	cfg.pollInterval = time.Duration(pollIntervalSec) * time.Second
 
+	if val, ok := os.LookupEnv(addressEnvParamName); ok {
+		cfg.address = val
+	}
+
+	if val, ok := os.LookupEnv(baseEnvParamName); ok {
+		cfg.base = val
+	}
+
+	if val, ok := os.LookupEnv(pollIntervalEnvParamName); ok {
+		pollInterval, err := getDuration(val, pollIntervalEnvParamName)
+		if err != nil {
+			return nil, err
+		}
+		cfg.pollInterval = pollInterval
+	}
+
+	if val, ok := os.LookupEnv(reportIntervalEnvParamName); ok {
+		reportInterval, err := getDuration(val, reportIntervalEnvParamName)
+		if err != nil {
+			return nil, err
+		}
+		cfg.reportInterval = reportInterval
+	}
+
 	return cfg, nil
+}
+
+func getDuration(val, paramName string) (time.Duration, error) {
+	intVal, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return 0 * time.Second, fmt.Errorf("erorr while parsing param %s. Got value: %s. Error: %s", paramName, val, err.Error())
+	}
+
+	return time.Duration(intVal) * time.Second, nil
 }
