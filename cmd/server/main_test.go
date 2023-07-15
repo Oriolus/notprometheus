09f6c547de00f35c86415832980639c3f28eb82c
Ivan Sinitsin <isinitsin@ozon.ru>
2023-07-15 22:31:13 +0400

    [iter3] add tests

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -37,7 +37,12 @@ func TestHandlers(t *testing.T) {
 	}{
 		{"", http.MethodGet, "", http.StatusOK},
 		{"/update/counter/cnt/1", http.MethodPost, "", http.StatusOK},
+		{"/value/counter/cnt", http.MethodGet, "1", http.StatusOK},
+		{"/value/counter/cnt11", http.MethodGet, "", http.StatusNotFound},
 		{"/update/gauge/g/1.0", http.MethodPost, "", http.StatusOK},
+		{"/value/gauge/g", http.MethodGet, "1", http.StatusOK},
+		{"/value/gauge/g11", http.MethodGet, "", http.StatusNotFound},
+		{"/value/gg/g11", http.MethodGet, "", http.StatusNotFound},
 		{"/update/gg", http.MethodPost, "404 page not found\n", http.StatusNotFound},
 		{"/update/gauge/g", http.MethodPost, "404 page not found\n", http.StatusNotFound},
 	}
